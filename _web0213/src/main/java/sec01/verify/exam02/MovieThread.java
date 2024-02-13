2f7c450fdfb8a1dd5b38c40680bb8607bad79499
Jiwoong-Jung <jjw981119@naver.com>
2024-02-13 15:33:02 +0900

    20240213 자바 스레드

--- _web0213/src/main/java/sec01/verify/exam02/MovieThread.java
+++ _web0213/src/main/java/sec01/verify/exam02/MovieThread.java
@@ -8,7 +8,7 @@ public class MovieThread extends Thread {
 	@Override
 	public void run() {
 		for (int i = 0; i < 3; i++) {
-			System.out.println("동영상 재생");
+			System.out.println(this.getName());
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
--- _web0213/src/main/java/sec01/verify/exam02/ThreadEx.java
+++ _web0213/src/main/java/sec01/verify/exam02/ThreadEx.java
@@ -3,11 +3,16 @@ package sec01.verify.exam02;
 public class ThreadEx {
 
 	public static void main(String[] args) {
+		Thread mth = Thread.currentThread();
+		System.out.println(mth.getName());
+		
 		Thread th1 = new MovieThread();
+		System.out.println(th1.getName());
 		th1.start();
 		
 		Thread th2 = new Thread(new MusicRunnable());
 		th2.setName("Music");
+		System.out.println(th2.getName());
 		th2.start();
 	}
 
