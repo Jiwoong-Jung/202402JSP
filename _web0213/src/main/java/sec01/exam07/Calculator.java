8bca5f9502fbd577748d59264f7408a32cb1fa21
Jiwoong-Jung <jjw981119@naver.com>
2024-02-13 15:40:15 +0900

    20240213 자바 스레드

---
+++ _web0213/src/main/java/sec01/exam07/Calculator.java
@@ -0,0 +1,23 @@
+package sec01.exam07;
+
+public class Calculator {
+	private int memory;
+
+	public int getMemory() {
+		return memory;
+	}
+
+	public void setMemory(int memory) {
+		this.memory = memory;
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println(Thread.currentThread().getName() + ": "+this.memory);
+	}
+	
+	
+
+}
--- _web0213/src/main/java/sec01/verify/exam02/ThreadEx.java
+++ _web0213/src/main/java/sec01/verify/exam02/ThreadEx.java
@@ -7,12 +7,12 @@ public class ThreadEx {
 		System.out.println(mth.getName());
 		
 		Thread th1 = new MovieThread();
-		System.out.println(th1.getName());
+		System.out.println(th1.getName()+"***");
 		th1.start();
 		
 		Thread th2 = new Thread(new MusicRunnable());
 		th2.setName("Music");
-		System.out.println(th2.getName());
+		System.out.println(th2.getName()+"***");
 		th2.start();
 	}
 
