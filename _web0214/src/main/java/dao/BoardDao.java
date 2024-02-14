c7159009e54a2f97c026263be01f7948b92bdc1b
Jiwoong-Jung <jjw981119@naver.com>
2024-02-14 10:33:15 +0900

    20240214 DAO

--- _web0214/src/main/java/dao/BoardDao.java
+++ _web0214/src/main/java/dao/BoardDao.java
@@ -1,9 +1,21 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class BoardDao {
 	private static BoardDao dao = new BoardDao();
 	private BoardDao() {} // 생성자
 	public static BoardDao getInstance() {
 		return dao;
 	}
+	
+	private Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection conn = DriverManager.getConnection(
+        		"jdbc:mysql://localhost:3306/project1", "root", "mysql");
+		return conn;
+	}
+	
 }
