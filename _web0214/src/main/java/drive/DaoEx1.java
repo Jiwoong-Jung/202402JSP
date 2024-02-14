255c291cb7e7da7900e9985ea98535377d102749
Jiwoong-Jung <jjw981119@naver.com>
2024-02-14 10:51:50 +0900

    20240214 DAO test

---
+++ _web0214/src/main/java/drive/DaoEx1.java
@@ -0,0 +1,16 @@
+package drive;
+
+import java.util.List;
+
+import dao.BoardDao;
+import dto.Board;
+
+public class DaoEx1 {
+
+	public static void main(String[] args) {
+		BoardDao dao = BoardDao.getInstance();
+		List<Board> list = null; // selectList() 호출해 보세요.
+		// list에 들어있는 객체들을 출력해 보세요.
+	}
+
+}
