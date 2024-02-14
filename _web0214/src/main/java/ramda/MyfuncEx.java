77b255b790b63d530103f461f601d801c5a4208a
Jiwoong-Jung <jjw981119@naver.com>
2024-02-14 16:19:26 +0900

    20240214 람다식 기본

--- _web0214/src/main/java/ramda/MyfuncEx.java
+++ _web0214/src/main/java/ramda/MyfuncEx.java
@@ -5,8 +5,9 @@ public class MyfuncEx {
 	public static void main(String[] args) {
 		MyFunc mf, mf3;
 		
-		mf3 =(a) -> System.out.println(a);  //람다식
-		mf3.method(20);
+		mf3 = x -> {
+			System.out.println(x);
+		};
 		
 		mf = new MyFunc() {  // 익명 객체 생성
 			
