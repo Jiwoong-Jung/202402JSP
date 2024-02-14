0b9f337f0614a8a85843b5bb8fead8ba2dc41060
Jiwoong-Jung <jjw981119@naver.com>
2024-02-14 10:46:34 +0900

    20240214 DAO

--- _web0214/src/main/java/dao/BoardDao.java
+++ _web0214/src/main/java/dao/BoardDao.java
@@ -2,7 +2,12 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+
+import dto.Board;
 
 public class BoardDao {
 	private static BoardDao dao = new BoardDao();
@@ -18,4 +23,34 @@ public class BoardDao {
 		return conn;
 	}
 	
+	public ArrayList<Board> selectList() {
+		ArrayList<Board> list = new ArrayList<Board>();
+		String sql = "select * from board order by num desc";
+		PreparedStatement pstmt;
+		try {
+			pstmt = this.getConnection().prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			
+			while (rs.next()) {
+				Board board = new Board(rs.getInt("num"), rs.getString("writer"), 
+						rs.getString("title"), rs.getString("content"),
+						rs.getString("regtime"), 0);
+				list.add(board);
+			}
+		} catch (ClassNotFoundException | SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return list;
+		
+	}
+	
 }
+
+
+
+
+
+
