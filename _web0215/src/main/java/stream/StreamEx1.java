af0cd3454579b5edc7cc9c1ed810bf391d4ea4b2
Jiwoong-Jung <jjw981119@naver.com>
2024-02-16 15:48:55 +0900

    스트림 API

--- _web0215/src/main/java/stream/StreamEx1.java
+++ _web0215/src/main/java/stream/StreamEx1.java
@@ -1,22 +1,35 @@
 package stream;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class StreamEx1 {
 
 	public static void main(String[] args) {
 		List<Integer> list 
 		          = Arrays.asList(1, 2, 3, 4, 5);
+		list
+		.stream()
+//		.filter(i->i%2==0)
+		.map(i->i*2)
+		.forEach(i->System.out.println(i));
+		
+//		List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5);
+//		Stream<Integer> stream = integerList.stream(); // Stream 생성
+//		Stream<Integer> stream2 = stream.filter(i -> i % 2 == 0); // 중간 연산
+//		stream2.forEach(i -> System.out.println(i)); // 최종 연산
+		
+		
+		
 //			= new ArrayList<>();
 //		list.add(1);list.add(2);
 //		list.add(3);list.add(4);list.add(5);
-		for (Integer i : list) {
-		    if (i % 2 == 0) { // 2의 배수 추출
-		        System.out.println(i);
-		    }
-		}
+//		for (Integer i : list) {
+//		    if (i % 2 == 0) { // 2의 배수 추출
+//		        System.out.println(i);
+//		    }
+//		}
 
 	}
 
