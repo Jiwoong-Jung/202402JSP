3a5138f52f5a4c7accbd8359e644c811fb871963
Jiwoong-Jung <jjw981119@naver.com>
2024-02-19 16:25:29 +0900

    MemberDao에 selectMembers() HashMap

---
+++ _web0219/src/main/java/collections/HashMapEx2.java
@@ -0,0 +1,41 @@
+package collections;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Scanner;
+
+import dao.MemberDao;
+import dto.Member;
+
+public class HashMapEx2 {
+
+	public static void main(String[] args) {
+		MemberDao dao = MemberDao.getInstance();
+		HashMap<String, Member> map = dao.selectMembers();
+		
+
+		Scanner scanner = new Scanner(System.in);
+		while (true) {
+			System.out.println("아이디와 비밀번호 입력");
+			System.out.print("아이디: " );
+			String id = scanner.nextLine();
+			System.out.print("비밀번호: " );
+			String password = scanner.nextLine();
+			System.out.println();
+			
+			if (map.containsKey(id)) { // 좋은 메소드
+				if (map.get(id).getEmail().equals(password)) {
+					System.out.println("로그인됨");
+					break;
+				} else {
+					System.out.println("비밀번호 오류");
+				}
+			} else {
+				System.out.println("입력한 아이디가 존재 안함");
+			}
+			
+		}
+	}
+
+}
