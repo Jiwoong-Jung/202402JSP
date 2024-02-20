b621aa79d39c8f433725105f5d1618e2ea978a6b
Jiwoong-Jung <jjw981119@naver.com>
2024-02-20 16:25:19 +0900

    서블릿 process()

--- _web0220/src/main/java/servlet/DispatcherServlet.java
+++ _web0220/src/main/java/servlet/DispatcherServlet.java
@@ -26,16 +26,22 @@ public class DispatcherServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		process(request, response);
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		process(request, response);
+	}
+	
+	private void process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		
 	}
 
 }
+
+
+
+
