713d4de0c5f88a7c406ac288aa48b2784928e8f8
Jiwoong-Jung <jjw981119@naver.com>
2024-02-21 16:19:10 +0900

    loginForm.jsp 작성

--- _web0221/src/main/java/controller/DispatcherServlet.java
+++ _web0221/src/main/java/controller/DispatcherServlet.java
@@ -19,64 +19,63 @@ import dto.Board;
 @WebServlet("*.do")
 public class DispatcherServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public DispatcherServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public DispatcherServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		process(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		process(request, response);
 	}
-	
-	private void process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-			String uri = request.getRequestURI();
-			String path = uri.substring(uri.lastIndexOf("/"));
-			if (path.equals("/list.do")) {
-				BoardDao dao = BoardDao.getInstance();
-				ArrayList<Board> list = dao.selectList();
-				// 포워딩 작업
-				request.setAttribute("list", list);
-				RequestDispatcher dispatcher
-				    = request.getRequestDispatcher("/WEB-INF/view/list.jsp");
-				dispatcher.forward(request, response);
-				
-			} else if (path.equals("/view.do")) {
-				int num = Integer.parseInt(request.getParameter("num"));
-				BoardDao dao = BoardDao.getInstance();
-				Board board = dao.selectOne(num, true);
-				// 글 제목과 내용이 웹 페이지에 올바르게 출력되도록 
-			    // 공백과 줄 바꿈 처리
-				String title = board.getTitle().replace(" ", "&nbsp;");
-				board.setTitle(title);
-			    
-			    String content = board.getContent()
-			    		 .replace(" ", "&nbsp;").replace("\n", "<br>");
-			    board.setContent(content);
-				
-				// 포워딩 작업
-				request.setAttribute("bd", board);
-				RequestDispatcher dispatcher
-				    = request.getRequestDispatcher("/WEB-INF/view/view.jsp");
-				dispatcher.forward(request, response);
-			}
+
+	private void process(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		String uri = request.getRequestURI();
+		String path = uri.substring(uri.lastIndexOf("/"));
+		if (path.equals("/list.do")) {
+			BoardDao dao = BoardDao.getInstance();
+			ArrayList<Board> list = dao.selectList();
+			// 포워딩 작업
+			request.setAttribute("list", list);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/list.jsp");
+			dispatcher.forward(request, response);
+
+		} else if (path.equals("/view.do")) {
+			int num = Integer.parseInt(request.getParameter("num"));
+			BoardDao dao = BoardDao.getInstance();
+			Board board = dao.selectOne(num, true);
+			// 글 제목과 내용이 웹 페이지에 올바르게 출력되도록
+			// 공백과 줄 바꿈 처리
+			String title = board.getTitle().replace(" ", "&nbsp;");
+			board.setTitle(title);
+
+			String content = board.getContent().replace(" ", "&nbsp;").replace("\n", "<br>");
+			board.setContent(content);
+
+			// 포워딩 작업
+			request.setAttribute("bd", board);
+			RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/view/view.jsp");
+			dispatcher.forward(request, response);
+		}
 	}
 
 }
-
-
-
-
---
+++ _web0221/src/main/webapp/loginForm.jsp
@@ -0,0 +1,16 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="login.do" method="post"> 
+아이디:<input type="text" name="id"><br>
+이메일:<input type="text" name="email"><br>
+<input type="submit" value="로그인">
+</form>
+</body>
+</html>
