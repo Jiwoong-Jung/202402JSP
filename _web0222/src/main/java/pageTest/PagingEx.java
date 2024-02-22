d9f9b89e42a2c3b8178eb571701e99db4811e180
Jiwoong-Jung <jjw981119@naver.com>
2024-02-22 14:07:06 +0900

    페이징 테스트

---
+++ _web0222/src/main/java/pageTest/PagingEx.java
@@ -0,0 +1,20 @@
+package pageTest;
+
+public class PagingEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static void pageTest(int currentPage, int totalPages) {
+		int modVal = currentPage % 5;
+		int startPage = currentPage / 5 * 5 + 1;
+		if (modVal == 0) startPage -= 5;
+		
+		//startPage, endPage: 화면 하단에 보여줄 페이지 이동 링크의 시작 번호와 끝 번호
+		int endPage = startPage + 4;
+		if (endPage > totalPages) endPage = totalPages;
+		System.out.println("startPage: " + startPage +", endPage: " + endPage);
+	}
+
+}
