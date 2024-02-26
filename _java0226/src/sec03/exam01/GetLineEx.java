ea7f51a891321baf487fa3dcd8bdaaf14ab2db47
Jiwoong-Jung <jjw981119@naver.com>
2024-02-26 15:29:08 +0900

    입출력 API

--- _java0226/src/sec03/exam01/GetLineEx.java
+++ _java0226/src/sec03/exam01/GetLineEx.java
@@ -6,16 +6,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Scanner;
 
 public class GetLineEx {
 
 	public static void main(String[] args) throws IOException {
-		InputStream is = System.in;
-		Reader reader = new InputStreamReader(is);
-		BufferedReader br = new BufferedReader(reader);
+//		InputStream is = System.in;
+//		Reader reader = new InputStreamReader(is);
+//		BufferedReader br = new BufferedReader(reader);
+//		BufferedReader br 
+//		 = new BufferedReader(new InputStreamReader(System.in));
+		Scanner br = new Scanner(System.in);
 		while(true){
 			System.out.print("입력하세요: ");
-			String lineStr = br.readLine();
+//			String lineStr = br.readLine();
+			String lineStr = br.nextLine();
 			if (lineStr.equals("q") || lineStr.equals("quit")) {
 				break;
 			}
