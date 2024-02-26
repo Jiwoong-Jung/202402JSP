56d3eca02f3448fb9ed5d5f6e88478e889dbb8e9
Jiwoong-Jung <jjw981119@naver.com>
2024-02-26 15:49:50 +0900

    입출력 API

--- _java0226/src/sec03/exam03/FileEx.java
+++ _java0226/src/sec03/exam03/FileEx.java
@@ -1,14 +1,18 @@
 package sec03.exam03;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class FileEx {
 
 	public static void main(String[] args) {
 		File dir = new File("c:/temp");
 		File[] list = dir.listFiles();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 		for (File file : list) {
-			System.out.println(file);
+			System.out.println(
+					sdf.format(new Date(file.lastModified())));
 		}
 
 	}
