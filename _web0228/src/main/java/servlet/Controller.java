f251c73e18f127106a05cf0bd54b0cb8b4e8e824
Jiwoong-Jung <jjw981119@naver.com>
2024-02-29 14:09:11 +0900

    서블릿으로 컨트롤러 만들기

---
+++ _web0228/src/main/java/servlet/Controller.java
@@ -0,0 +1,52 @@
+package servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("*.do")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Controller() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		process(request, response);
+	}
+	private void process(HttpServletRequest request, HttpServletResponse response) {
+		String uri = request.getRequestURI();
+		System.out.println(uri);
+		String path = uri.substring(uri.lastIndexOf("/"));
+		System.out.println(path);
+	}
+
+}
+
+
+
+
+
+
+
